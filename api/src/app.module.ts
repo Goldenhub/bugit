f805ab9daffcfeb25f6ccecb127b148c7be6ec2c
Goldenhub <goldenazubuike@gmail.com>
2026-06-17 19:02:05 +0100

    fix(api): implemented a /health enpoint for pinging

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,8 +6,10 @@ import { CommentsModule } from './comments/comments.module';
 import { MetaModule } from './meta/meta.module';
 import { AuthModule } from './auth/auth.module';
 import { ApiTokenGuard } from './auth/api-token.guard';
+import { HealthController } from './health.controller';
 
 @Module({
+  controllers: [HealthController],
   imports: [
     MongooseModule.forRoot(
       process.env.MONGODB_URI ?? 'mongodb://localhost:27017/bugit',
